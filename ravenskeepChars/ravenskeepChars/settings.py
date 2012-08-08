411a1da25dbd237623d484f758cd1445723dccf4
Rogier van het Schip <rogier@euclides.(none)>
2012-08-08 21:25:59 +0200

    App chars toegevoegd

--- ravenskeepChars/ravenskeepChars/settings.py
+++ ravenskeepChars/ravenskeepChars/settings.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    'chars',
 )
 
 # A sample logging configuration. The only tangible logging
