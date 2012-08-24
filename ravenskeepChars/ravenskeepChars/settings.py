e04d14dce6e6cfb610394e64c789c1fda33454f9
Rogier van het Schip <rogiervanhetschip@gmail.com>
2012-08-24 09:44:09 +0200

    Made login required, added login template, added login view to urls.py, added logout view

--- ravenskeepChars/ravenskeepChars/settings.py
+++ ravenskeepChars/ravenskeepChars/settings.py
@@ -132,6 +132,10 @@ INSTALLED_APPS = (
     'chars',
 )
 
+LOGIN_URL = '/ravenskeepChars/login/'
+
+LOGIN_REDIRECT_URL = '/ravenskeepChars/'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
