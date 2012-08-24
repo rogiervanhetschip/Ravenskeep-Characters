ce95533d3e9dcb7e6a4c8786b8a845dc161f9844
Rogier van het Schip <rogiervanhetschip@gmail.com>
2012-08-24 16:51:01 +0200

    Added postgresql as development server

--- ravenskeepChars/ravenskeepChars/settings.py
+++ ravenskeepChars/ravenskeepChars/settings.py
@@ -18,17 +18,26 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
 
 if bool(os.environ.get('LOCAL_DEV', False)):
     DATABASES = {
         'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(SITE_ROOT, 'db') + '/development.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'ravenskeepCharsDb',                      # Or path to database file if using sqlite3.
+        'USER': 'ravenskeepCharsDbUser',                      # Not used with sqlite3.
+        'PASSWORD': 'ravenskeepniveau',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': os.path.join(SITE_ROOT, 'db') + '/development.db',                      # Or path to database file if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
