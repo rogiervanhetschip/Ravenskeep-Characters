21391fe58bdea3d3850779027a0c21fa69a9320d
Rogier van het Schip <rogiervanhetschip@gmail.com>
2013-06-17 07:57:50 +0200

    Named URLs

--- chars/urls.py
+++ chars/urls.py
@@ -1,21 +1,22 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 
 urlpatterns = patterns('chars.views',
-    url(r'^$', 'home'),
-    url(r'^char/(?P<char_id>\d+)/$', 'charRead'),
-    # url(r'^char/(?P<char_id>\d+)/create$', 'charCreate'),
-    url(r'^char/(?P<char_id>\d+)/read/$', 'charRead'),
-    url(r'^char/(?P<char_id>\d+)/printpreview/$', 'charPrintPreview'),
-    # url(r'^char/(?P<char_id>\d+)/edit$', 'charEdit'),
-    # url(r'^char/(?P<char_id>\d+)/delete$', 'charDelete'),
+    url(r'^$', 'home', name='home'),
+    url(r'^char/(?P<char_id>\d+)/$', 'charRead', name='charRead'),
+    # url(r'^char/(?P<char_id>\d+)/create$', 'charCreate', name='charCreate'),
+    url(r'^char/(?P<char_id>\d+)/read/$', 'charRead', name='charRead'),
+    url(r'^char/(?P<char_id>\d+)/printpreview/$', 'charPrintPreview', name='charPrintPreview'),
+    # url(r'^char/(?P<char_id>\d+)/edit$', 'charEdit', name='charEdit'),
+    # url(r'^char/(?P<char_id>\d+)/delete$', 'charDelete', name='charDelete'),
 
     # Logout
-    (r'^logout/$', 'logout_user'),
+    url(r'^logout/$', 'logout_user', name='logout'),
 
 )
 
 urlpatterns += patterns('',
     # Login
-    (r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
 )
 
