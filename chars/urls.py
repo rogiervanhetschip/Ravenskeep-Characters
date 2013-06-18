fb6143d6b77a96bcf44396d80fcd008c29791404
Rogier van het Schip <rogiervanhetschip@gmail.com>
2013-06-18 18:23:17 +0200

    No separate edit URL, removed superfluous read URL

--- chars/urls.py
+++ chars/urls.py
@@ -4,10 +4,8 @@ urlpatterns = patterns('chars.views',
     url(r'^$',                                    'home',              name='home'),
     url(r'^char/(?P<char_id>\d+)/$',              'charRead',          name='charRead'),
     # url(r'^char/(?P<char_id>\d+)/create$', 'charCreate', name='charCreate'),
-    url(r'^char/(?P<char_id>\d+)/read/$',         'charRead',          name='charRead'),
     url(r'^char/(?P<char_id>\d+)/printpreview/$', 'charPrintPreview',  name='charPrintPreview'),
     url(r'^char/printpreview/$',                  'charsPrintPreview', name='charsPrintPreview'),
-    # url(r'^char/(?P<char_id>\d+)/edit$', 'charEdit', name='charEdit'),
     # url(r'^char/(?P<char_id>\d+)/delete$', 'charDelete', name='charDelete'),
 
     # Logout
