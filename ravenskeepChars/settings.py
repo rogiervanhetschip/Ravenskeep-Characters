9d81f1c6eb228856ed7e78073047d276c7b662bb
Rogier van het Schip <rogiervanhetschip@gmail.com>
2013-06-18 19:32:54 +0200

    Added main menu to login page, fixed login button

--- ravenskeepChars/settings.py
+++ ravenskeepChars/settings.py
@@ -149,7 +149,7 @@ INSTALLED_APPS = (
 
 LOGIN_URL = '/login/'
 
-LOGIN_REDIRECT_URL = '/ravenskeepChars/'
+LOGIN_REDIRECT_URL = '/'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
