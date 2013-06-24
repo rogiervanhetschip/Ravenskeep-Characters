0d3cd8d79aa7a897e1271a8b576bc95f9594c154
Rogier van het Schip <rogiervanhetschip@gmail.com>
2013-06-24 19:14:29 +0200

    Index page with table

--- chars/views.py
+++ chars/views.py
@@ -1,36 +1,33 @@
-from django.shortcuts import render_to_response, get_object_or_404, redirect
+from django.shortcuts import render_to_response, get_object_or_404, redirect, render
 from django.core.context_processors import csrf
-from chars.models import Character
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from chars.models import Character
 import pdb
 
 @login_required
 def home(request):
-    c = {}
-    c.update(csrf(request))
-    if request.method == 'POST':
-        if request.POST['oldornew'] == 'old':
-            return redirect('char/' + request.POST['charId'] + '/printpreview/', c)
-        return redirect('char/' + request.POST['charId'], c)
-    return render_to_response('index.html', c)
+  c = {}
+  c.update(csrf(request))
+  chars = Character.objects.filter(dood=False) # Evt. later values() gebruiken, maar dan moet method 
+  return render(request, 'index.html', { 'chars': chars, 'c': c })
 
 @login_required
 def charRead(request, char_id):
-    char = get_object_or_404(Character, id=char_id)
-    return render_to_response('char.html', {'char': char})
+  char = get_object_or_404(Character, id=char_id)
+  return render_to_response('char.html', {'char': char})
 
 @login_required
 def charPrintPreview(request, char_id):
-    char = get_object_or_404(Character, id=char_id)
-    return render_to_response('charPrintPreview.html', {'chars': { char } })
+  char = get_object_or_404(Character, id=char_id)
+  return render_to_response('charPrintPreview.html', {'chars': { char } })
 
 @login_required
 def charsPrintPreview(request, chars):
-    return render_to_response('charPrintPreview.html', {'chars': chars})
+  return render_to_response('charPrintPreview.html', {'chars': chars})
 
 @login_required
 def logout_user(request):
-    logout(request)
-    return redirect('/login/')
+  logout(request)
+  return redirect('/login/')
 
