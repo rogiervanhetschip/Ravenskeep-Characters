069cb5a63deec94d4aac64293914731d2dcea92c
Rogier van het Schip <rogiervanhetschip@gmail.com>
2013-07-25 17:03:21 +0200

    Django-chosen for ManyToMany fields, only static is not detected

--- chars/forms.py
+++ chars/forms.py
@@ -1,7 +1,20 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, DateInput, SelectMultiple
 from chars.models import Character
+from chosen.forms import ChosenSelectMultiple
+import pdb
 
 class CharacterForm(ModelForm):
+  # http://stackoverflow.com/questions/4101258/how-do-i-add-a-placeholder-on-a-charfield-in-django
+  def __init__(self, *args, **kwargs):
+    super(CharacterForm, self).__init__(*args, **kwargs)
+    for field_name in self.fields:
+      field = self.fields.get(field_name)  
+      if field:
+        if type(field.widget) in (TextInput, DateInput): # TODO: DateInput verwisselen met een TextInput?!
+          field.widget = TextInput(attrs={'placeholder': field.label})
+        elif type(field.widget) is SelectMultiple:
+          field.widget = ChosenSelectMultiple(overlay=field.help_text, choices=field.choices)
+
   class Meta:
     model = Character
 
